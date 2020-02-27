6a55458e6972933dd3ee7aa91bbb807ead47819b
FabrizioMontanari <ffabrizio.montanari@gmail.com>
2020-02-27 17:35:50 +0100

    fixed error for small base10 values

--- pyAIC/__init__.py
+++ pyAIC/__init__.py
@@ -48,7 +48,7 @@ def from10to32(string):
     str
         The converted string.
     """
-
+    
     res = ""
     remainder = int(string)
     while remainder > 31:
@@ -56,7 +56,10 @@ def from10to32(string):
         remainder = remainder//32
         res = res + char
     res = res + AIC_TABLE[remainder].upper()
-    return res[::-1]
+    res = res[::-1]
+    if len(res) == 5:
+        res = "0" + res
+    return res
 
 
 def check_AIC_base10_checksum(AIC):
